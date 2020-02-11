b411adefa61bf1a10bc0ff9a01c55d51c7d67fe3
Opilane TTHK <opilane@tthk.ee>
2020-02-11 10:54:06 +0200

    the finale

--- server.js
+++ server.js
@@ -3,16 +3,31 @@ const port = 3000
 const express = require('express')
 const app = express();
 
+app.set('view engine', 'ejs');
+
 function getDayOfWeek(date){
-    var daysOfWeek = ["Sunday you're stuck on my mind", "Monday comes and my eyes are darting all around me", "Tuesday i see you", "Wednesday everything slows down", "Thursday i'd like to get to know you", "Friday i haven't recovered", "Saturday you're stuck on my mind"]
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     
     return daysOfWeek[date.getDay()];
 }
 
 
-app.get('/', (req,res) => {
+app.get('/today', (req,res) => {
     var today = new Date();
-    res.send(getDayOfWeek(today));
+    //res.send(getDayOfWeek(today));
+    res.render('dayOfWeek', {
+        date: today,
+        dayOfWeek: getDayOfWeek(today)
+    });
+})
+
+app.get('/birthday', (req,res) => {
+    var birthday = new Date(2002, 5, 17);
+    //res.send(getDayOfWeek(today));
+    res.render('dayOfWeek', {
+        date: birthday,
+        dayOfWeek: getDayOfWeek(birthday)
+    });
 })
 
 app.listen(port, () => console.log(`Started listening on port ${port}`))
